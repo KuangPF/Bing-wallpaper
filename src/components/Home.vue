7cd18cb5d28d71af7128ac2ce2d0a3486250fea4
kuangpengfei <kuangpengfei@youloft.com>
2017-12-08 15:38:10 +0800

    preDay nextDay today

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,12 +6,13 @@
         <div class="icon-menu" @click="menuShow"></div>
         <div class="mask" v-if="maskShow" @click="menuHide"></div>
         <div class="slide-bar" :class="{'slide-bar-show' : slideBarShow==1}">
-        </div>
-        <!-- <div class="switch-congtent">
+            <div class="switch-congtent">
             <div class="preDay" @click="getPreDay">前一天</div>
             <div class="nextDay" @click="getNextDay">后一天</div>
             <div class="today" @click="getToday">今天</div>
-        </div> -->
+        </div>
+        </div>
+        
     </div>
 </template>
 
@@ -41,16 +42,34 @@ export default {
     menuHide() {
       this.maskShow = false;
       this.slideBarShow = 0;
+      localStorage.removeItem("preDay_idx");
     },
     getPreDay() {
-      getBingWallpaper(1).then(res => {
-        console.log(res);
-        console.log(res);
+      let idx = 0; //请求的天数序列 0代表今天、1代表昨天…以此类推
+      let idx_storage = localStorage.getItem("preDay_idx");
+      if (idx_storage) {
+        idx = idx_storage;
+      }
+      idx++;
+      localStorage.setItem("preDay_idx", idx);
+      console.log(idx);
+      getBingWallpaper(idx).then(res => {
         this.bingWallpaperUrl = res;
       });
     },
-    getNextDay() {},
-    getToday() {}
+    getNextDay() {
+      let idx = localStorage.getItem("preDay_idx");
+      idx--;
+      localStorage.setItem("preDay_idx", idx);
+      getBingWallpaper(idx).then(res => {
+        this.bingWallpaperUrl = res;
+      });
+    },
+    getToday() {
+      getBingWallpaper(0).then(res => {
+        this.bingWallpaperUrl = res;
+      });
+    }
   }
 };
 </script>
